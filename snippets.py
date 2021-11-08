2f0ec237b71c2935303349f3e1f041886f0d53c8
leidashuaige <370998447@qq.com>
2021-11-08 11:11:05 +0800

    修改文件路径

--- snippets.py
+++ snippets.py
@@ -11,8 +11,8 @@ import jieba
 from bert4keras.snippets import open
 
 # 自定义词典
-user_dict_path = '/root/cail2020/sfzyx/datasets/user_dict.txt'
-user_dict_path_2 = '/root/cail2020/sfzyx/datasets/user_dict_2.txt'
+user_dict_path = '/content/spaces/datasets/user_dict.txt'
+user_dict_path_2 = '/content/spaces/datasets/user_dict_2.txt'
 jieba.load_userdict(user_dict_path)
 jieba.initialize()
 
@@ -20,21 +20,21 @@ jieba.initialize()
 sys.setrecursionlimit(1000000)
 
 # 标注数据
-data_json = '/root/cail2020/sfzyx/datasets/train.json'
+data_json = '/content/spaces/datasets/train.json'
 
 # 保存权重的文件夹
 if not os.path.exists('weights'):
     os.mkdir('weights')
 
 # bert配置
-config_path = '/root/kg/bert/chinese_roberta_wwm_ext_L-12_H-768_A-12/bert_config.json'
-checkpoint_path = '/root/kg/bert/chinese_roberta_wwm_ext_L-12_H-768_A-12/bert_model.ckpt'
-dict_path = '/root/kg/bert/chinese_roberta_wwm_ext_L-12_H-768_A-12/vocab.txt'
+config_path = '/content/drive/MyDrive/chinese_roberta_wwm_ext_L-12_H-768_A-12/bert_config.json'
+checkpoint_path = '/content/drive/MyDrive/chinese_roberta_wwm_ext_L-12_H-768_A-12/bert_model.ckpt'
+dict_path = '/content/drive/MyDrive/chinese_roberta_wwm_ext_L-12_H-768_A-12/vocab.txt'
 
 # nezha配置
-nezha_config_path = '/root/kg/bert/nezha_base/bert_config.json'
-nezha_checkpoint_path = '/root/kg/bert/nezha_base/model.ckpt-900000'
-nezha_dict_path = '/root/kg/bert/nezha_base/vocab.txt'
+nezha_config_path = '/content/drive/MyDrive/NEZHA-Base/bert_config.json'
+nezha_checkpoint_path = '/content/drive/MyDrive/NEZHA-Base/model.ckpt-900000'
+nezha_dict_path = '/content/drive/MyDrive/NEZHA-Base/vocab.txt'
 
 # 将数据划分N份，一份作为验证集
 num_folds = 15
